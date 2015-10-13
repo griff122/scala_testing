38d47ecc52ac7cbbeff9cab8963285f367e83fde
Ryan Griffin <rgriffin@in4ins.com>
2015-10-13 13:11:05 -0400

    Added bot infix and regular notation.

--- ImportJava/src/FrenchDate.scala
+++ ImportJava/src/FrenchDate.scala
@@ -8,6 +8,7 @@ object FrenchDate {
         val df = getDateInstance(LONG, Locale.FRANCE)
         // Note: Expressions requiring only one argument can be used with an infix syntax:
         //      That is, df format now, is equivalent to df.format(now)
-        println(df format now) 
+        println("Infix notation:" + (df format now))
+        println("Regular notation:" + df.format(now))
     }
 }
